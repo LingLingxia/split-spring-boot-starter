3cbe84e0791c5d5bd8206c56545e1bab278f0cd8
LingLingxia <llxlansing@qq.com>
2020-11-02 21:40:16 +0800

    init

---
+++ src/main/java/com/imooc/springboot/service/ISplitService.java
@@ -0,0 +1,7 @@
+package com.imooc.springboot.service;
+
+import java.util.List;
+
+public interface ISplitService {
+    List<String> split(String value);
+}
---
+++ src/main/java/com/imooc/springboot/service/impl/SplitServiceImpl.java
@@ -0,0 +1,17 @@
+package com.imooc.springboot.service.impl;
+
+import com.imooc.springboot.service.ISplitService;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class SplitServiceImpl implements ISplitService {
+    @SuppressWarnings("all")
+    @Override
+    public List<String> split(String value) {
+        return Stream.of(StringUtils.split(value,","))
+                .collect(Collectors.toList());
+    }
+}
